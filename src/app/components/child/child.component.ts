efa597f41c519da9d911ad838fcf2f14da0cbe58
Surya Madhavan <surya.madhavan@capgemini.com>
2023-01-24 16:49:32 +0530

    Changes 24 - 01 - 23

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, ContentChild, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
-  constructor() {}
+export class ChildComponent implements OnInit, OnDestroy, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterContentChecked {
+  constructor() {
+    console.log("Child constructor ");
+  }
+
+  @Input()
+  channelName = "";
+
+  @ContentChild("projected")
+  projected: any;
 
   ngOnInit(): void {
+    console.log("Child OnInit - ", this.projected);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
+    console.log("Child OnChanges - ", this.projected);
+  }
+
+  ngDoCheck() {
+    console.log("Child DoCheck - ", this.projected);
     
+  }
+
+  ngAfterContentInit() {
+    console.log("Child AfterContentInit - ", this.projected);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log("Child After Content Checked - ", this.projected);
+  }
+
+  ngAfterViewInit(): void {
+    console.log("Child After View Init");
+    
+  }
+
+  ngAfterViewChecked(): void {
+    console.log("Child After View Checked");
+    
+  }
+
+  ngOnDestroy(): void {
+    console.log("Child OnDestroy");
   }
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit, OnChanges, DoCheck {
+  constructor() {
+    console.log("Parent constructor ");
+  }
+  isChild: boolean = true;
 
+
+  channelName = "";
+  ngOnInit(): void {
+    console.log("Parent OnInit");
+  }
+
+  ngOnChanges() {
+    console.log("Parent OnChanges");
+  }
+
+  ngDoCheck() {
+    console.log("Parent DoCheck");
+  }
+
+
+  toggleChild() {
+    this.isChild = !this.isChild;
+  }
 }
